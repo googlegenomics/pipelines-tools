27e6b3fe6cfdcfa394371f739dfe3f1af4a18996
Ana Manolache <anamano@google.com>
2018-06-05 14:41:49 -0400

    review changes

--- ssh-server/main.go
+++ ssh-server/main.go
@@ -10,8 +10,12 @@ import (
 	"os"
 	"os/exec"
 
+	"os/signal"
+	"syscall"
+
 	"github.com/kr/pty"
 	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 const PWD_ENV = "SSH_PWD"
@@ -73,46 +77,74 @@ func setupAuthentication(password string) (*ssh.ServerConfig, error) {
 }
 
 func handleConnection(nConn net.Conn, serverConfig *ssh.ServerConfig) {
+	// Before use, a handshake must be performed on the incoming
+	// net.Conn.
 	_, chans, reqs, err := ssh.NewServerConn(nConn, serverConfig)
 	if err != nil {
 		fmt.Errorf("failed to handshake, %v", err)
 	}
 
+	// Service the incoming request channel so connection doesn't hang
 	go ssh.DiscardRequests(reqs)
 
+	// Service the incoming channels
 	for newChannel := range chans {
-		if newChannel.ChannelType() != "session" {
-			newChannel.Reject(ssh.UnknownChannelType, "unknown channel type")
-			continue
-		}
-
-		channel, requests, err := newChannel.Accept()
-		if err != nil {
-			fmt.Errorf("Could not accept channel: %v", err)
-		}
-		defer channel.Close()
-
-		// Start the command with a pseudo-terminal.
-		bashPTY, err := pty.Start(exec.Command("bash"))
-		if err != nil {
-			fmt.Errorf("Could not start pty: %v", err)
-		}
-		// Redirect pseudo-terminal output to client channel
-		go func() {
-			io.Copy(bashPTY, channel)
-			channel.Close()
-		}()
-		// Redirect client channel input to pseudo-terminal
-		go func() {
-			io.Copy(channel, bashPTY)
-			channel.Close()
-		}()
-
-		go func(in <-chan *ssh.Request) {
-			for req := range in {
-
-				req.Reply(req.Type == "pty-req" || req.Type == "shell", nil)
-			}
-		}(requests)
+		serviceChannel(newChannel)
 	}
+}
+
+func serviceChannel(newChannel ssh.NewChannel) {
+	if newChannel.ChannelType() != "session" {
+		newChannel.Reject(ssh.UnknownChannelType, "unknown channel type")
+		return
+	}
+	channel, requests, err := newChannel.Accept()
+	if err != nil {
+		fmt.Errorf("Could not accept channel: %v", err)
+	}
+	defer channel.Close()
+
+	// Tell the client we accept pty and shell commands
+	go func(in <-chan *ssh.Request) {
+		for req := range in {
+			req.Reply(req.Type == "pty-req" || req.Type == "shell", nil)
+		}
+	}(requests)
+
+	// Start the command with a pseudo-terminal.
+	bashPTY, err := pty.Start(exec.Command("bash"))
+	if err != nil {
+		fmt.Errorf("Could not start pty: %v", err)
+	}
+	defer bashPTY.Close()
+
+	// Resize pseudo terminal so the terminal pointer will be at the correct position
+	resizePTY(bashPTY)
+
+	// Redirect pseudo-terminal output to client channel
+	go func() {
+		io.Copy(bashPTY, channel)
+	}()
+	// Redirect client channel input to pseudo-terminal
+	io.Copy(channel, bashPTY)
+}
+
+func resizePTY(bashPTY *os.File) {
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGWINCH)
+	go func() {
+		for range ch {
+			if err := pty.InheritSize(os.Stdin, bashPTY); err != nil {
+				log.Printf("error resizing pty: %s", err)
+			}
+		}
+	}()
+	ch <- syscall.SIGWINCH
+	// Initial resize.
+	// Set stdin in raw mode.
+	oldState, err := terminal.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
+		panic(err)
+	}
+	defer func() { _ = terminal.Restore(int(os.Stdin.Fd()), oldState) }()
 }
