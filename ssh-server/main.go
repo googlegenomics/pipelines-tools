bde08c52a1b620afda4561a257a2a2a5ab8a46e6
Ana Manolache <anamano@google.com>
2018-11-20 15:31:57 -0500

    fixed browser ssh by unblocking the channel handling

--- ssh-server/main.go
+++ ssh-server/main.go
@@ -11,7 +11,6 @@ import (
 	"io"
 	"log"
 	"net"
-	"os"
 	"os/exec"
 
 	"github.com/googlegenomics/pipelines-tools/gce"
@@ -31,6 +30,7 @@ func main() {
 		log.Fatalf("Failed to start server: %v", err)
 	}
 	log.Printf("Listening on %s...", listener.Addr())
+	defer listener.Close()
 
 	for {
 		connection, err := listener.Accept()
@@ -39,7 +39,9 @@ func main() {
 		}
 
 		go func() {
-			handleConnection(connection, config)
+			if err := handleConnection(connection, config); err != nil {
+				log.Printf("Handling connection: %v", err)
+			}
 		}()
 	}
 }
@@ -86,18 +88,21 @@ func getConfiguration() (*ssh.ServerConfig, error) {
 }
 
 func handleConnection(conn net.Conn, serverConfig *ssh.ServerConfig) error {
-	_, chans, reqs, err := ssh.NewServerConn(conn, serverConfig)
+	sConn, chans, reqs, err := ssh.NewServerConn(conn, serverConfig)
 	if err != nil {
 		return fmt.Errorf("handshake: %v", err)
 	}
+	defer sConn.Close()
 
 	// Discard out-of-band SSH requests (not supported by this server).
 	go ssh.DiscardRequests(reqs)
 
 	for newChannel := range chans {
-		if err := serviceChannel(newChannel); err != nil {
-			return err
-		}
+		go func() {
+			if err := serviceChannel(newChannel); err != nil {
+				log.Printf("Failed to service channel: %v", err)
+			}
+		}()
 	}
 	return nil
 }
@@ -111,40 +116,78 @@ func serviceChannel(newChannel ssh.NewChannel) error {
 	if err != nil {
 		return fmt.Errorf("accepting channel: %v", err)
 	}
-	defer channel.Close()
 
-	// Start the command with a pseudo-terminal.
+	allow := map[string]bool{"shell": true, "exec": true, "pty-req": true, "window-change": true}
+	resize := make(chan *pty.Winsize, 1)
+	done := make(chan struct{})
+	for {
+		select {
+		case <-done:
+			return nil
+		case req, ok := <-requests:
+			if !ok {
+				return nil
+			}
+			if req.WantReply {
+				req.Reply(allow[req.Type], nil)
+			}
+			switch req.Type {
+			case "pty-req":
+				go func() {
+					newPTY(channel, resize)
+					close(done)
+				}()
+
+				skip := binary.BigEndian.Uint32(req.Payload)
+				size, err := windowSize(req.Payload[4+skip:])
+				if err != nil {
+					log.Printf("Failed to get window size: %v", err)
+					continue
+				}
+				resize <- size
+			case "window-change":
+				size, err := windowSize(req.Payload)
+				if err != nil {
+					log.Printf("Failed to get window size: %v", err)
+					continue
+				}
+				resize <- size
+			}
+		}
+	}
+}
+
+func newPTY(channel ssh.Channel, resize chan *pty.Winsize) {
+	//Start the command with a pseudo-terminal.
 	shell, err := pty.Start(exec.Command("bash"))
 	if err != nil {
-		return fmt.Errorf("starting pty: %v", err)
+		log.Printf("starting pty: %v", err)
 	}
 	defer shell.Close()
 
-	go func(in <-chan *ssh.Request) {
-		for req := range in {
-			// Tell the client we accept pty and shell commands
-			req.Reply(req.Type == "pty-req" || req.Type == "shell", nil)
-
-			if req.Type == "pty-req" {
-				skip := binary.BigEndian.Uint32(req.Payload)
-				resize(shell, req.Payload[4+skip:])
-			}
-			if req.Type == "window-change" {
-				resize(shell, req.Payload)
-			}
+	go func() {
+		for size := range resize {
+			pty.Setsize(shell, size)
 		}
-	}(requests)
+	}()
 
 	go io.Copy(shell, channel)
 	io.Copy(channel, shell)
-	return nil
+
+	status := struct{ Status uint32 }{uint32(0)}
+	if _, err := channel.SendRequest("exit-status", false, ssh.Marshal(&status)); err != nil {
+		log.Printf("Failed to send exit request: %v", err)
+	}
+	channel.Close()
 }
 
-func resize(f *os.File, payload []byte) {
+func windowSize(payload []byte) (*pty.Winsize, error) {
 	var size struct {
 		Width, Height uint32
 	}
 	if err := binary.Read(bytes.NewReader(payload), binary.BigEndian, &size); err == nil {
-		pty.Setsize(f, &pty.Winsize{Cols: uint16(size.Width), Rows: uint16(size.Height)})
+		return &pty.Winsize{Cols: uint16(size.Width), Rows: uint16(size.Height)}, nil
+	} else {
+		return nil, err
 	}
 }
